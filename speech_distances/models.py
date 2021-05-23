406588bfb69802bd27a3b80c3d81854e50c41618
Pavel Andreev <andreev.pk@phystech.edu>
2021-05-23 18:06:07 +0300

    Added DeepSpeech2

--- speech_distances/models.py
+++ speech_distances/models.py
@@ -1,6 +1,8 @@
 """This is a module for model load. If possible, use nemo or torch model zoo. 
 Imports are hided in if statements for efficiency. 
 """
+import os.path
+import wget
 
 
 def load_model(name: str, device="cpu"):
@@ -10,5 +12,13 @@ def load_model(name: str, device="cpu"):
     elif name.lower() == 'quartznet':
         import nemo.collections.asr as nemo_asr
         return nemo_asr.models.EncDecCTCModel.from_pretrained(model_name="QuartzNet15x5Base-En", map_location=device)
-    else:
-        raise NotImplementedError
+    elif name.lower() == 'deepspeech2':
+        from .deep_speech import DeepSpeechEncoderWrapper
+        if os.path.isfile('weights/an4_pretrained_v2.pth'):
+             return DeepSpeechEncoderWrapper("weights/an4_pretrained_v2.pth", device=device)
+        else:
+            os.mkdir("weights")
+            wget.download("https://github.com/SeanNaren/deepspeech.pytorch/releases/download/v2.0/an4_pretrained_v2.pth",
+                          out="weights")
+            return DeepSpeechEncoderWrapper("weights/an4_pretrained_v2.pth", device=device)
+            
